eb5da5979fb944ac5a8b86db4b1669b55138fa3c
Christianmagni97 <christianmagni28@gmail.com>
2024-05-23 17:08:12 +0200

    prove primo snack

---
+++ script.js
@@ -0,0 +1,9 @@
+const invited = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
+invited. forEach((element, indice) →{
+    console.log(element, indice);
+});
+    
+const TavoloVip = invited.map((element) → {
+    return prodotto + '9c' + 'black'
+
+});
