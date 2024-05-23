90a934f6891ccc58b8ceb834392fb9025a7049e7
Christianmagni97 <christianmagni28@gmail.com>
2024-05-23 18:33:51 +0200

    corretot .map

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const invited = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
-invited. forEach((element, indice) →{
-    console.log(element, indice);
+const invited = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
+    invited. forEach((element) =>{
+    console.log(element);
 });
-    
-const TavoloVip = invited.map((element) → {
-    return element + '9c' + 'black'
 
+const TavoloVip = invited.map((element) => {
+    return element + ' 9c ' + ' black ';
 });
+console.log(TavoloVip);
